ce534e0d0b204fa58ed8ae9383c01cc1ee80bd63
izuna <dkapitsev@gmail.com>
2022-12-08 19:26:23 +0700

    added jenkins pipeline.groovy

---
+++ Jenkins/test_pipeline.groovy
@@ -0,0 +1,21 @@
+properties([
+    parameters([
+        choice(
+            name: "Test Param 1",
+            choices: [
+                "Test Param Choise 1",
+                "Test Param Choise 2"
+            ],
+            description: ""
+            ),
+        string(name: 'Version', defaultValue: '', description: 'available version')    
+        ])
+    ])
+node ('Ubuntu Main'){
+    stage("Test stage 1"){
+            println("---->")
+            println("Stage 1 is ok")
+            println("---->")
+        
+    }
+}
