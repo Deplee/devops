e714b60f4561abb875c4c54cca19dc88d1494271
izuna <dkapitsev@gmail.com>
2022-12-15 22:47:04 +0700

    added test lib pipe

---
+++ Jenkins/test-lib-pipe.groovy
@@ -0,0 +1,9 @@
+@Library("my-shared-lib") _
+
+node("Ubuntu Main"){
+    stage("test-1"){
+    println("STAGE #1")
+        mapTest.map(test: "David", age: "32")
+        mapTest.call("ivan")
+    }
+}
